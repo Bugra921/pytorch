0d47cda6702a5ae2e3fcb8bb742327499221fdc9
Bugra921 <bugrakoca115@gmail.com>
2024-11-25 23:32:35 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,45 +1,29 @@
 import streamlit as st
 import torch
-import torch.nn as nn
+import torchvision.models as models
 import torchvision.transforms as transforms
 from PIL import Image
 import numpy as np
 import cv2
 from io import BytesIO
 
-# Model mimarisini tanımlayın
-class MyModel(nn.Module):
-    def __init__(self):
-        super(MyModel, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(128 * 56 * 56, 256)  # 224x224 giriş boyutu olduğunu varsayarsak
-        self.fc2 = nn.Linear(256, 3)  # 3 sınıf olduğunu varsayarsak
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
-        x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+class_size = 3
+model = models.efficientnet_v2_s(weights='DEFAULT')
+model.classifier[1] = torch.nn.Linear(1280, class_size)
 
 # Cihazı ayarlayın (GPU varsa kullan, yoksa CPU kullan)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
 
-# Modeli oluşturun ve yükleyin
-model = MyModel()
+# Modeli yükleyin
 model.load_state_dict(torch.load("best_model.pth", map_location=device))
-model.to(device)
 model.eval()
 
 # Görüntüyü işleme fonksiyonu
 def preprocess_image(img):
     transform = transforms.Compose([
         transforms.ToPILImage(),
-        transforms.Resize((224, 224)),
+        transforms.Resize((256, 256)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
@@ -54,7 +38,6 @@ def predict_image(img):
         outputs = model(img)
     _, predicted = torch.max(outputs, 1)
     return predicted.cpu().numpy()[0]
-    
 
 # Streamlit arayüzü
 st.title("Fasulye Hastalığı Tespit Uygulaması")
